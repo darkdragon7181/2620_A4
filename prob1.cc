0a420d2af2e33a7f386c5e2517fc2ecbfe6c3026
Navraj Saini <navraj.saini@uleth.ca>
2017-03-08 04:37:31 -0700

    done the main file

--- prob1.cc
+++ prob1.cc
@@ -4,9 +4,75 @@
 int main()
 {
    LineEditor line;
-   cout << line;
+   string command;
+   string s;
+   char c;
    
-   line.change(c);
-   
+   cin >> command;
+
+   while(command != 'Q' || 'q')
+   {
+      switch(command)
+      {
+	 case 'L' :
+	 {
+	    line.left();
+	    break;
+	 }
+	 case 'R' :
+	 case 'r' :
+	    {
+	       line.right();
+	       break;
+	    }
+	 case 'I' :
+	 case 'i' :
+	    {
+	       cin >> s;
+	       line.insert(s);
+	       break;
+	    }
+	 case 'D' :
+	 case 'd' :
+	    {
+	       line.erase();
+	       break;
+	    }
+	 case 'C' :
+	 case 'c' :
+	    {
+	       cin >> c;
+	       line.change(c);
+	       break;
+	    }
+	 case 'U' :
+	 case 'u' :
+	    {
+	       //upper case
+	       line.apply();
+	       break;
+	    }
+	 case 'l' :
+	 {
+	    //lower case
+	    line.apply();
+	    break;
+	 }
+	 case 'P' :
+	 case 'p' :
+	    {
+	       ostream os;
+	       os << line;
+	       break;
+	    }
+	 case 'Q' :
+	 case 'q' :
+	    {
+	       return 0;
+	       break;
+	    }
+      }
+      cin >> command;
+   }
    return 0;
 }
